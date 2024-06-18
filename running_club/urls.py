238f1f359212e9bcfd050332000ff6a5896f69e4
Brian Mc Conway <brianmcconway@yahoo.co.uk>
2024-06-18 11:45:43 +0000

    Add 'members' app and add to installed apps, create view, add to urls and test.

--- running_club/urls.py
+++ running_club/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from members import views as members_views
 
 urlpatterns = [
+    path('members/', members_views.my_members, name='members'),
     path('admin/', admin.site.urls),
 ]
