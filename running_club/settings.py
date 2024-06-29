ffda83a6583104f05078cc4e551a401dacbf0a5f
Brian Mc Conway <brianmcconway@yahoo.co.uk>
2024-06-29 16:14:27 +0000

    Add STATICFILES_STORAGE to settings.py, debug=false for test deployment of fixes.

--- running_club/settings.py
+++ running_club/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-brianmcconw-brendandoyl-6erdwlly20g.ws.codeinstitute-ide.net','.herokuapp.com',]
 
@@ -144,6 +144,8 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-brianmcconw-brendandoyl-6erdwlly20g.ws.codeinstitute-ide.net',
