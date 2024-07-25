594862a9bcac07086922bad64c80af34f2335d68
Brian Mc Conway <brianmcconway@yahoo.co.uk>
2024-07-25 09:52:49 +0000

    Fix map attempting to load on all pages, create & add mapId, DEBug=False for deployment.

--- static/js/script.js
+++ static/js/script.js
@@ -1,31 +1,31 @@
 async function initMap() {
-    try {
-        const { Map } = await google.maps.importLibrary("maps");
-        const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
-        const map = new Map(document.getElementById('map'), {
-            zoom: 10,
-            center: { lat: 53.7272, lng: -7.7938 }, // Centered over Longford, Ireland
-            mapId: 'DEMO_MAP_ID' // Replace with your actual map ID
-        });
-
-        const locations = [
-            { lat: 53.725090, lng: -7.800639 }, // Harbour Row location
-            { lat: 53.669318, lng: -7.985161 }, // Lanesboro Track location
-            { lat: 53.732445, lng: -7.790903 }  // Mall Complex location
-        ];
-
-        locations.forEach((location, i) => {
-            new AdvancedMarkerElement({
-                map: map,
-                position: location,
-                title: `Location ${i + 1}`
-            });
-        });
-    } catch (error) {
-        console.error("Failed to initialize map: ", error);
+    if (!document.getElementById('map')) {
+        console.warn("Map element not found, skipping initialization.");
+        return;
     }
+
+    const { Map } = await google.maps.importLibrary("maps");
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+    const map = new Map(document.getElementById('map'), {
+        zoom: 10,
+        center: { lat: 53.7272, lng: -7.7938 }, // Centered over Longford, Ireland
+        mapId: '26eb5394bc1b80ae' // Replace with your actual map ID
+    });
+
+    const locations = [
+        { lat: 53.725090, lng: -7.800639 }, // Harbour Row location
+        { lat: 53.669318, lng: -7.985161 }, // Lanesboro Track location
+        { lat: 53.732445, lng: -7.790903 }  // Mall Complex location
+    ];
+
+    locations.forEach((location, i) => {
+        new AdvancedMarkerElement({
+            position: location,
+            title: `Location ${i + 1}`,
+            map: map
+        });
+    });
 }
 
 window.initMap = initMap;
-
