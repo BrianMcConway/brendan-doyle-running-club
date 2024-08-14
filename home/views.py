4933f42c9997c81a0b2de1b9184b50d98b8c0862
Brian Mc Conway <brianmcconway@yahoo.co.uk>
2024-08-14 15:39:24 +0000

    Restore imports in views, adjust baxckground images for mobile view, collectstatic.

--- home/views.py
+++ home/views.py
@@ -1,16 +1,34 @@
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.conf import settings
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
 def home(request):
+    """
+    Render the home page.
+    """
     return render(request, 'home/index.html')
 
 def about(request):
+    """
+    Render the about page.
+    """
     return render(request, 'home/about.html')
 
 def classes_view(request):
+    """
+    Render the classes page with Google Maps API key in context.
+    """
     context = {
         'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY,
     }
     return render(request, 'home/classes.html', context)
 
 def contact(request):
+    """
+    Render the contact page.
+    """
     return render(request, 'home/contact.html')
 
 def contact_view(request):
@@ -31,7 +49,12 @@ def contact_view(request):
             subject = f'Message from {name}'
             content = f'Name: {name}\nEmail: {email}\n\nMessage:\n{message}'
 
-            mail = Mail(from_email=from_email, to_emails=to_email, subject=subject, plain_text_content=content)
+            mail = Mail(
+                from_email=from_email,
+                to_emails=to_email,
+                subject=subject,
+                plain_text_content=content
+            )
             try:
                 response = sg.send(mail)
                 print(response.status_code)
@@ -46,4 +69,7 @@ def contact_view(request):
     return render(request, 'home/contact.html', {'form': form})
 
 def contact_success_view(request):
+    """
+    Render the contact success page.
+    """
     return render(request, 'home/contact_success.html')
